bdd92cb467e5446b46405a411baae91e325dff03
muamarzidan <codeofomiru11@gmail.com>
2024-07-20 08:37:38 +0700

    feat: add link to login

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -12,15 +12,15 @@ const Footer = () => {
                         </div>
                         <div id="menu" className="order-2 sm:order-1 w-full sm:w-[33%] flex flex-col gap-2">
                             <p className="text-xl font-bold text-white sm:text-lg xl:text-2xl 2xl:text-3xl"><a href="/">Home</a></p>
-                            <p className="text-xl font-bold text-white sm:text-lg xl:text-2xl 2xl:text-3xl"><a href="about">About Us</a></p>
+                            <p className="text-xl font-bold text-white sm:text-lg xl:text-2xl 2xl:text-3xl"><a href="/about">About Us</a></p>
                             <p className="text-xl font-bold text-white sm:text-lg xl:text-2xl 2xl:text-3xl"><a href="/faq">FAQ</a></p>
                         </div>
                         <div id="sosial-media" className="order-3 w-full sm:w-[33%] flex flex-col justify-around gap-2">
-                            <div id="sosmed-insta" className="flex items-center gap-10">
+                            <div id="sosmed-insta" className="flex items-center gap-10 cursor-pointer">
                                 <Icon icon="f7:logo-instagram" width={42} height={42} style={{ color: "white" }} />
                                 <p className="text-xl font-bold text-white sm:text-lg xl:text-2xl 2xl:text-3xl">symbiosix2024</p>
                             </div>
-                            <div id="sosmed-wa" className="flex items-center gap-10">
+                            <div id="sosmed-wa" className="flex items-center gap-10 cursor-pointer">
                                 <Icon icon="formkit:whatsapp" width={42} height={42} style={{ color: "white" }} />
                                 <p className="text-xl font-bold text-white sm:text-lg xl:text-2xl 2xl:text-3xl">+62 814-1081-4644</p>
                             </div>
