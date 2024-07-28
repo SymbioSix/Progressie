053c192ac9116e951bf3bbdf3cecac329c099c2d
muamarzidan <codeofomiru11@gmail.com>
2024-07-28 19:43:44 +0700

    test

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  // resolve: {
+  //   alias: {
+  //     '@': fileURLToPath(new URL('./src', import.meta.url))
+  //   }
+  // },
+  build: {
+    outDir: "<PATH TO OUT DIR ATAU HASIL BUILD>"
+  },
+  base:"/Progressie"
 })
