be448f802c5d2beebae2a348b04214ff1e317222
muamarzidan <codeofomiru11@gmail.com>
2024-08-16 17:50:50 +0700

    configuring

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,8 +1,7 @@
-// src/utils/request.js
 import axios from "axios";
 
 const apiSettings = axios.create({
-  baseURL: import.meta.env.BASE_URL,
+  baseURL: import.meta.env.VITE_BASE_URL,
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
@@ -12,5 +11,28 @@ const apiSettings = axios.create({
     "Access-Control-Allow-Credentials": "true",
   },
 });
+
+apiSettings.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("authToken");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+apiSettings.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default apiSettings;
