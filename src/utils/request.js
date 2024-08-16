6c3542c5223bb7ecd2e1910a822be4ced988d491
muamarzidan <codeofomiru11@gmail.com>
2024-08-16 17:37:07 +0700

    configuring

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,7 @@
+// src/utils/request.js
 import axios from "axios";
 
-const api = axios.create({
+const apiSettings = axios.create({
   baseURL: import.meta.env.BASE_URL,
   timeout: 10000,
   headers: {
@@ -12,4 +13,4 @@ const api = axios.create({
   },
 });
 
-export default api;
+export default apiSettings;
