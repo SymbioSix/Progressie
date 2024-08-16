9109b5c5f221aab6ee70a4bc79d5d3dccd9d92ce
muamarzidan <codeofomiru11@gmail.com>
2024-08-17 00:07:53 +0700

    feat: adjust style in profile navbar

--- src/pages/dashboard/components/header.jsx
+++ src/pages/dashboard/components/header.jsx
@@ -5,6 +5,7 @@ import { Icon } from "@iconify/react";
 
 const Header = () => {
   const [isDropdown, setIsDropdown] = useState(false);
+  
   const toggleDropdown = () => {
     setIsDropdown(!isDropdown);
   };
@@ -24,26 +25,37 @@ const Header = () => {
           <div className="relative w-auto h-auto ">
             <button
               onClick={toggleDropdown}
-              className="flex items-center w-full h-full gap-3 px-2 py-3 bg-white rounded-md"
+              className="flex items-center w-full h-full gap-3 px-2 py-3 rounded-md"
             >
               <Icon
                 icon="mdi:account-box-outline"
                 width="30"
                 height="30"
-                style={{ color: "#000000" }}
+                style={{ color: "#ffffff" }}
               />
               <div className="flex flex-col items-start">
-                <span className="text-sm font-bold">User Name</span>
-                <span className="text-sm font-bold">(Admin)</span>
+                <span className="text-sm font-bold text-white">User Name</span>
+                <span className="text-sm font-bold text-white">(Admin)</span>
               </div>
-              <Icon
-                icon="iconamoon:arrow-up-2-fill"
-                width="26"
-                height="26"
-                style={{ color: "#000000" }}
-              />
+              {
+                isDropdown ? (
+                  <Icon
+                    icon="mdi:chevron-up"
+                    width="26"
+                    height="26"
+                    style={{ color: "#ffffff" }}
+                  />
+                ) : (
+                  <Icon
+                    icon="mdi:chevron-down"
+                    width="26"
+                    height="26"
+                    style={{ color: "#ffffff" }}
+                  />
+                )
+              }
               {isDropdown && (
-                <div className="absolute top-[65px] right-[-2px] w-[180px] py-[10px] px-[5px] bg-[#F7EBE6] border border-gray-200 rounded-md shadow-md font-bold">
+                <div className="absolute top-[65px] right-[-2px] w-[180px] py-[10px] px-[5px] bg-white border border-gray-200 rounded-md shadow-md font-bold">
                   <ul className="space-y-2">
                     <li className="w-full flex items-center justify-start px-3 py-2 rounded-[5px]">
                     <Icon icon="pajamas:settings" width="26" height="26"  style={{color: "#000000"}} />
